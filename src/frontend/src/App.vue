dfabe83883073881489a258b2f913c969185bcd8
RFieret <494690@student.fontys.nl>
2023-03-30 21:02:29 +0200

    Updated to a new repo. Added a nav bar with log in options.

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <nav-bar />
   <HelloWorld/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HelloWorld
   }
 }
